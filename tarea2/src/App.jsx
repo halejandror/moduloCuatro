12a27741b707ee8a8036b3b7ef0a8207b985f12c
AlejandroRodriguez <usuariolg80@gmail.com>
2024-07-28 07:26:27 -0500

    subo tarea-2 perosnaje favorito

---
+++ tarea2/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import  PruebaComponent  from './components/pruebaComponent';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        JESUS DE NAZARET
+      </h1>
+      <img src='./jesus.png' alt='personaje'/>
+      <PruebaComponent />
+    </div>
+  );
+}
+
+export default App;
